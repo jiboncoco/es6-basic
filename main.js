e17168addac92bc5bcf5ada1a8ca75d5ac427432
Achmad Fauzi <achmadfauzi@live.com>
2020-08-29 02:23:46 +0700

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+// ES5
+
+var tahun2020 = "dapat memiliki mobil dengan cash tanpa kredit";
+
+tahun2020 = "dapat memiliki rumah dan mobil dengan cash tanpa kredit";
+console.log(tahun2020);
+
+// ES6
+
+const tahun2021 = "dapat memiliki rumah 2 lantai dan mobil dengan cash tanpa kredit";
+console.log(tahun2021);
+
+let diTahun2021 = "bisa pergi haji bersama keluarga & istri";
+
+diTahun2021 = "nikah di bulan june 2020";
+console.log(diTahun2021);
